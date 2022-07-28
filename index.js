c893d22543eb74fc27f06cecbcdb4cb420098225
Cristian Falcone <cristianfalcone@gmail.com>
2022-07-28 16:57:21 +0000

    Merge new implementation

--- index.js
+++ index.js
@@ -1,206 +1,203 @@
-const { isArray } = Array
-const { hasOwn, keys } = Object
+export const
+  Fragment = ({ children }) => children,
 
-export const Skip = Symbol()
-export const Fragment = ({ children }) => children
-export const Portal = ({ host, children }) => render(host, children)
+  h = (tagName, props, ...children) => {
+    children = children.length == 0 ? null : children.length == 1 ? children[0] : children
+    return { children, ...props, tagName }
+  },
 
-const Element = Symbol()
+  component = setup => ({ is, key, ref, host, ...props }) => h(is ?? setup.is ?? 'div', {
+    key, ...setup.host, ...host, skip: true, ref: host => (refresh(host, props, setup), isFn(ref) && ref(host))
+  }),
 
-export const createTextNode = data => ({ [Element]: '#text', data, skip: true })
+  render = (h, host) => {
 
-export const createElement = (name, props, ...children) => {
-  props = { ...props, [Element]: name }
-  children = props.children ?? children
-  if (children.length > 0) props.children = children.length === 1 ? children[0] : children
-  return props
-}
+    let child = host.firstChild, node, byKey = keyed.get(host)
 
-export const isElement = el => hasOwn(el ?? {}, Element)
+    for (h of normalize(h, host)) {
 
-function* g(vnode, host) {
-  const vnodes = isArray(vnode) ? vnode : [vnode]
-  let data = ''
+      if (typeof h == str) {
 
-  while (vnodes.length > 0) {
-    vnode = vnodes.shift()
+        for (node = child; node; node = node.nextSibling) if (node.nodeType == 3) break
+        node ? node.data !== h && (node.data = h) : node = document.createTextNode(h)
 
-    if (vnode == null || typeof vnode === 'boolean') continue
-    if (typeof vnode === 'string') data += vnode
-    else if (isElement(vnode)) {
-      const { [Element]: name } = vnode
+      } else if (hasOwn(h, 'tagName')) {
 
-      if (typeof name === 'function') {
-        vnodes.unshift(name(vnode, host))
-        continue
-      }
+        const { tagName, key, skip, ref, children, block, ...props } = h
 
-      if (data) {
-        yield createTextNode(data)
-        data = ''
-      }
+        if (key != null && (node = byKey?.get(key)));
+        else for (node = child; node; node = node.nextSibling) if (node.localName == tagName) break
 
-      yield vnode
-    } else typeof vnode[Symbol.iterator] === 'function'
-      ? vnodes.unshift(...vnode)
-      : data += vnode
-  }
+        node ||= document.createElement(tagName)
 
-  if (data) yield createTextNode(data)
-}
+        key != null && (byKey ||= keyed.set(host, createMap(Map))).set(key, node)
 
-export const render = (vnode, host) => {
-  let child = host.firstChild
+        update(props, node)
 
-  for (const { [Element]: name, ref, skip, children, ...props } of g(vnode, host)) {
+        !(skip || block != null && every(deps.get(node), deps.set(node, block))) && render(children, node)
 
-    if (name === Skip) {
-      child = props.end ? null : child?.nextSibling ?? null
-      continue
+        isFn(ref) && ref(node)
+
+      } else node = h
+
+      node === child ? child = child.nextSibling : before(host, child, node)
     }
 
-    let node = child
-
-    while (node != null) {
-      if (node.nodeName.toLowerCase() === name && node.getAttribute?.('key') == props.key) break
-      node = node.nextSibling
+    while (child) {
+      const next = child.nextSibling
+      host.removeChild(child).nodeType == 1 && dispose(child)
+      child = next
     }
+  },
 
-    if (node == null) node = name === '#text'
-      ? document.createTextNode('')
-      : document.createElement(name)
-
-    patch(props, node)
-
-    if (typeof ref === 'function') ref(node)
-    else if (typeof ref === 'object' && ref !== null) ref.current = node
-
-    skip || render(children, node)
-
-    if (node === child) child = child.nextSibling
-    else if (node.contains?.(document.activeElement)) {
-      const { nextSibling } = node
-      let ref = child
-
-      while (ref != null && ref !== node) {
-        const next = ref.nextSibling
-        host.insertBefore(ref, nextSibling)
-        ref = next
-      }
-    } else host.insertBefore(node, child)
-  }
-
-  while (child != null) {
-    const { nextSibling } = child
-    if (child.nodeType === Node.ELEMENT_NODE) dispose(child)
-    host.removeChild(child)
-    child = nextSibling
-  }
-}
-
-const patch = (props, node) => {
-  if (node.nodeType === Node.TEXT_NODE) {
-    if (node.data !== props.data) node.data = props.data
-    return
-  }
-
-  for (const name of new Set([...node.getAttributeNames(), ...keys(props)])) {
-    let value = props[name]
-
-    if (name in node && !(typeof value === 'string' && typeof node[name] === 'boolean')) {
-      try {
-        if (node[name] !== value) node[name] = value
-        continue
-      } catch (err) { }
-    }
-
-    if (value === true) value = ''
-    else if (value == null || value === false) {
-      node.removeAttribute(name)
-      continue
-    }
-
-    if (node.getAttribute(name) !== value) node.setAttribute(name, value)
-  }
-}
-
-const components = new WeakMap
-
-export const createComponent = fn => ({ is, host, key, ...props }) =>
-  createElement(is ?? fn.is ?? 'div', {
-    ...fn.host, ...host, key, skip: true, ref: host => {
-      let cmp = components.get(host)
-      if (cmp == null) components.set(host, cmp = new Component(host, fn))
-      cmp.props = { ...fn.props, ...props }
-      cmp.update()
-    }
-  })
-
-class Component {
-  constructor(host, fn) {
-    this.host = host
-    this.fn = fn
-    this.props = null
-    this.it = null
-    this.err = false
-  }
-
-  update({ method = 'next', arg } = {}) {
-    if (this.host == null) return
-    if (this.err) try { this.it.return() } catch (e) { this.host = null } finally { return }
-
+  refresh = (host, props, setup) => {
     try {
-      if (this.it == null) {
-        const init = this.fn.call(this, this.props, this.host)
-        this.it = typeof init?.next === 'function' ? init : generate.call(this, init)
+      if (setup && !isFn(setup)) throwTypeError('Setup', setup, fn)
+      props = props ? memo.set(host, { ...setup?.props, ...props }) : memo.get(host) ?? {}
+      render((renders.get(host) ?? renders.set(host, setup(props, host)))(props, host), host)
+    } catch (error) {
+      propagate(host, error)
+    }
+  },
+
+  provide = (host, key, value) => (provisions.get(host) ?? provisions.set(host, createMap(Map))).set(key, value),
+
+  consume = (host, key, fallback) => {
+    let map
+    while (host) {
+      if ((map = provisions.get(host)) && map.has(key)) return map.get(key)
+      host = host.parentNode
+    }
+    return fallback
+  },
+
+  intercept = (host, interceptor) => {
+    if (!isFn(interceptor)) throwTypeError('Interceptor', interceptor, fn)
+    interceptors.set(host, interceptor)
+  },
+
+  propagate = (host, error) => {
+    for (let interceptor; host; host = host.parentNode)
+      if (interceptor = interceptors.get(host)) return render(host, interceptor(error))
+    throw error
+  },
+
+  cleanup = (host, cleaner) => {
+    if (!isFn(cleaner)) throwTypeError('Cleaner', cleaner, fn);
+    (cleaners.get(host) ?? cleaners.set(host, new Set)).add(cleaner)
+  },
+
+  clx = o => keys(o).filter(k => o[k]).join(' ') || null,
+  stx = o => entries(o).map(t => t.join(':')).join(';') || null,
+  keb = o => keys(o).reduce((r, k) => ((r[k.replace(search, replace).toLowerCase()] = o[k]), r), {})
+
+const
+  createMap = (constructor, ...args) => {
+    const instance = new constructor(...args)
+    const { set } = instance
+    instance.set = (key, value) => (set.call(instance, key, value), value)
+    return instance
+  },
+
+  throwTypeError = (name, value, expected) => {
+    throw new TypeError(`Expected ${name} to be of type ${expected}, got ${typeof value} instead`)
+  },
+
+  every = (a, b) => a === b || isArray(a) && isArray(b) && a.length == b.length && a.every((v, i) => v === b[i]),
+  apply = (o, { name, value }) => ((o[name] = value), o),
+  reduce = list => from(list).reduce(apply, {}),
+  isFn = v => typeof v == fn,
+
+  search = /([a-z0-9])([A-Z])/g,
+  replace = '$1-$2',
+  str = 'string',
+  fn = 'function',
+
+  { keys, entries, hasOwn } = Object,
+  { isArray, from } = Array,
+  wm = WeakMap,
+
+  deps = createMap(wm),
+  memo = createMap(wm),
+  keyed = createMap(wm),
+  cache = createMap(wm),
+  renders = createMap(wm),
+  cleaners = createMap(wm),
+  provisions = createMap(wm),
+  interceptors = createMap(wm),
+
+  normalize = function* (h, host) {
+
+    let type, buffer = ''
+
+    for (h of isFn(h?.[Symbol.iterator]) ? h : [h]) {
+
+      if (h == null || (type = typeof h) == 'boolean') continue
+
+      if (h instanceof Node || hasOwn(h, 'tagName')) {
+
+        if (isFn(h.tagName)) {
+          yield* normalize(h.tagName(h, host), host)
+          continue
+        }
+
+        if (buffer) {
+          yield buffer
+          buffer = ''
+        }
+
+        yield h
+        continue
       }
 
-      const { value, done } = this.it[method](arg)
-
-      render(value, this.host)
-      if (done) this.host = null
-    } catch (err) {
-      this.err = true
-      propagate(this.host.parentNode, err)
+      (type == str || !isFn(h[Symbol.iterator])) ? buffer += h : yield* normalize(h, host)
     }
-  }
 
-  *[Symbol.iterator]() {
-    while (this.host) yield this.props
-  }
-}
+    if (buffer) yield buffer
+  },
 
-function* generate(init) {
-  yield init
-  for (const props of this) yield this.fn.call(this, props, this.host)
-}
+  update = (props, host) => {
 
-const propagate = (el, err) => {
-  if (el == null) throw err
-  const cmp = components.get(el)
-  typeof cmp?.it?.throw === 'function'
-    ? cmp.update({ method: 'throw', arg: err })
-    : propagate(el.parentNode, err)
-}
+    const prev = cache.get(host) ?? (host.hasAttributes() ? reduce(host.attributes) : {})
 
-const dispose = el => {
-  for (const child of el.children) dispose(child)
-  components.get(el)?.update({ method: 'return' })
-}
+    for (const name in { ...prev, ...props }) {
 
-const provisions = new WeakMap
+      let value = props[name]
 
-export const provide = (host, key, value) => {
-  let map = provisions.get(host)
-  if (map == null) provisions.set(host, map = new Map)
-  map.set(key, value)
-}
+      if (value === prev[name]) continue
 
-export const consume = (host, key) => {
-  for (let node = host; node != null; node = node.parentNode) {
-    if (provisions.has(node)) {
-      const map = provisions.get(node)
-      if (map.has(key)) return map.get(key)
+      if (name.startsWith('set:')) {
+        host[name.slice(4)] = value
+        continue
+      }
+
+      if (value === true) value = ''
+      else if (value == null || value === false) {
+        host.removeAttribute(name)
+        continue
+      }
+
+      host.setAttribute(name, value)
     }
+
+    cache.set(host, props)
+  },
+
+  before = (host, child, node) => {
+    if (node.contains?.(document.activeElement)) {
+
+      const ref = node.nextSibling
+
+      while (child && child !== node) {
+        const next = child.nextSibling
+        host.insertBefore(child, ref)
+        child = next
+      }
+
+    } else host.insertBefore(node, child)
+  },
+
+  dispose = host => {
+    for (const child of host.children) dispose(child)
+    for (const cleaner of cleaners.get(host) ?? []) cleaner(host)
   }
-}
