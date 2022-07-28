76d74bbe86136afc720e52b9b5da806764ce2a6a
Cristian Falcone <cristianfalcone@gmail.com>
2022-07-28 16:58:01 +0000

    Update test cases

--- index.test.js
+++ index.test.js
@@ -1,38 +1,37 @@
 import 'backdom/register'
 import { suite } from 'uvu'
 import * as assert from 'uvu/assert'
-import { createComponent, createElement, isElement, render, } from './index.js'
+import { component, h, render } from './index.js'
 
 let it
 
 // ----------------------------------------------------------------------------
 
-it = suite('createElement')
+it = suite('h')
 
 it('should create empty vnode', () => {
-  const vnode = createElement('div')
-  assert.ok(isElement(vnode))
-  assert.equal(vnode, {})
+  const vnode = h('div')
+  assert.equal(vnode, { tagName: 'div', children: null })
 })
 
 it('should create vnode with props', () => {
-  assert.equal(createElement('div', { id: 'app' }), { id: 'app' })
+  assert.equal(h('div', { id: 'app' }), { tagName: 'div', children: null, id: 'app' })
 })
 
 it('should create vnode with one string child', () => {
-  assert.equal(createElement('div', null, 'foo'), { children: 'foo' })
+  assert.equal(h('div', null, 'foo'), { tagName: 'div', children: 'foo' })
 })
 
 it('should create vnode with one vnode child', () => {
-  const child = createElement('span')
-  const vnode = createElement('div', null, child)
-  assert.ok(isElement(vnode.children))
+  const child = h('span')
+  const vnode = h('div', null, child)
+  assert.equal(vnode.children, { tagName: 'span', children: null })
 })
 
 it('should allow children as prop', () => {
-  const child = createElement('span')
-  const vnode = createElement('div', { children: child })
-  assert.ok(isElement(vnode.children))
+  const child = h('span')
+  const vnode = h('div', { children: child })
+  assert.equal(vnode.children, { tagName: 'span', children: null })
 })
 
 it.run()
@@ -43,22 +42,21 @@ it = suite('render')
 
 it('should render a vnode', () => {
   const host = document.createElement('div')
-  render(createElement('div', { foo: 'bar' }, 'foobar'), host)
+  render(h('div', { foo: 'bar' }, 'foobar'), host)
   assert.snapshot(host.innerHTML, '<div foo="bar">foobar</div>')
 })
 
 it('should render a stateless component', () => {
   const host = document.createElement('div')
-  const Foo = ({ foo, children }) => createElement('div', { foo }, children)
-  debugger
-  render(createElement(Foo, { foo: 'bar' }, createElement('span', null, 'foobar')), host)
+  const Foo = ({ foo, children }) => h('div', { foo }, children)
+  render(h(Foo, { foo: 'bar' }, h('span', null, 'foobar')), host)
   assert.snapshot(host.innerHTML, '<div foo="bar"><span>foobar</span></div>')
 })
 
 it('should render a stateful component', () => {
   const host = document.createElement('div')
-  const Foo = createComponent(({ foo, children }) => createElement('div', { foo }, children))
-  render(createElement(Foo, { is: 'foo-component', foo: 'bar' }, createElement('span', null, 'foobar')), host)
+  const Foo = component(() => ({ foo, children }) => h('div', { foo }, children))
+  render(h(Foo, { is: 'foo-component', foo: 'bar' }, h('span', null, 'foobar')), host)
   assert.snapshot(host.innerHTML, '<foo-component><div foo="bar"><span>foobar</span></div></foo-component>')
 })
 
