ba51da8092961a46e3a51124d8cb78b345fb4e8c
Cristian Falcone <cristianfalcone@gmail.com>
2023-11-17 16:48:40 +0000

    Add new tests file and vite config

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      'ajo': './index.js'
+    }
+  },
+  esbuild: {
+    jsxFactory: 'h',
+    jsxFragment: 'Fragment',
+    jsxInject: `import { h, Fragment } from 'ajo'`
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'index.js'),
+      name: 'Ajo',
+    }
+  }
+})
